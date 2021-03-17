ecf071645b834350627278268cdce53cc379aba8
Jameel Jiwani <jameeljiwani@gmail.com>
2021-03-17 13:17:10 -0400

    Added optional chaining

--- index.js
+++ index.js
@@ -5,15 +5,8 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-// client.on('message', msg => {
-//     console.log(msg);
-//   if (msg.content === 'ping') {
-//     msg.reply('Pong!');
-//   }
-// });
-
 client.on('message', msg => {
-    if(msg.embeds[0].MessageEmbed.title.includes("master")) {
+    if(msg?.embeds[0]?.title.includes("master")) {
         msg.reply("You idiot! You have to make a PR!!")
     }
 })
