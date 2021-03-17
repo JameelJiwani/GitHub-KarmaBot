eaf74282c81a491a263bafb4b3843bc27d05e3e2
Jameel Jiwani <jameeljiwani@gmail.com>
2021-03-17 18:08:48 -0400

    imported models into index

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const Discord = require('discord.js');
 const { Sequelize } = require('sequelize');
+const User = require('./models/user')
+const Server = require('./models/server')
+const Membership = require('./models/membership')
 var config = require('./config/config');
 
 const client = new Discord.Client();
@@ -24,10 +27,40 @@ sequelize.authenticate().then(() => {
     console.error('Unable to connect to the database:', err);
 });
 
-client.on('message', msg => {
+client.on('message', async msg => {
     console.log(msg);
     if(msg?.embeds[0]?.title.includes("master")) {
-        msg.reply("You idiot! You have to make a PR!!")
+        const [user, createdUser] = await User.findOrCreate({
+            where: { discordUID: msg?.author?.id },
+            defaults: {
+                userName: msg?.author?.username,
+                discordUID: msg?.author?.id
+            }
+        });
+
+        const [server, createdServer] = await Server.findOrCreate({
+            where: { discordServerID: msg?.channel?.guild?.id },
+            defaults: {
+                discordServerID: msg?.channel?.guild?.id,
+                serverName: msg?.channel?.guild?.name
+            }
+        });
+
+        const [membership, createdMembership] = await Membership.findOrCreate({
+            where: { userId: user.id },
+            defaults: {
+                karma: 0,
+                serverId: server.id,
+                userId: user.id
+            }
+        });
+
+        await Membership.update({ karma: membership.karma - 1 }, {
+            where: {
+                id: membership.id
+            }
+        });
+        msg.reply("You idiot! You have to make a PR!!\n New Karma: " + membership.karma - 1);
     }
 })
 
