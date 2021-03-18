b8f22f61d4ec18b14d25c41f980daefbcd4970dc
Jameel Jiwani <jameeljiwani@gmail.com>
2021-03-18 11:01:27 -0400

    removed classes

--- models/membership.js
+++ models/membership.js
@@ -1,23 +1,31 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class Membership extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  };
-  Membership.init({
+// 'use strict';
+// const {
+//   Model
+// } = require('sequelize');
+// module.exports = (sequelize, DataTypes) => {
+//   class Membership extends Model {
+//     /**
+//      * Helper method for defining associations.
+//      * This method is not a part of Sequelize lifecycle.
+//      * The `models/index` file will call this method automatically.
+//      */
+//     static associate(models) {
+//       // define association here
+//     }
+//   };
+//   Membership.init({
+//     karma: DataTypes.INTEGER
+//   }, {
+//     sequelize,
+//     modelName: 'Membership',
+//   });
+//   return Membership;
+// };
+
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+	sequelize.define('membership', {
     karma: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'Membership',
-  });
-  return Membership;
+	});
 };
